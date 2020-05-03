8c6a981c5569f1f115625f2a42614508884aeb16
lazyos <applichic@gmail.com>
2020-05-03 16:06:17 +0200

    Improve the file explorer system.

--- lib/ui/components/file_explorer.dart
+++ lib/ui/components/file_explorer.dart
@@ -18,26 +18,44 @@ class FileExplorer extends StatefulWidget {
 class _FileExplorerState extends State<FileExplorer> {
   List<Widget> _generateElements() {
     List<Widget> elements = List();
+    var size = MediaQuery.of(context).size;
+    var nbLines = 0;
+    var index = 0;
 
+    // Displays each files and folders
     for (var element in widget.fileEntities) {
+      double x = index * 110.0;
+      double y = nbLines * 90.0;
+
+      // Set the position in X, Y
+      if (x >= size.width - 300) {
+        index = 0;
+        nbLines++;
+        y = nbLines * 70.0;
+        x = 0;
+      }
+
       // Only displays visible files
       var fileName = path.basename(element.path);
 
       if (fileName[0] != '.') {
+        var icon = Icons.insert_drive_file;
+
         if (FileSystemEntity.isDirectorySync(element.path)) {
-          var filePreview = FilePreview(
-            name: fileName,
-            icon: Icons.folder,
-          );
-          elements.add(filePreview);
-        } else {
-          var filePreview = FilePreview(
-            name: fileName,
-            icon: Icons.insert_drive_file,
-          );
-          elements.add(filePreview);
+          icon = Icons.folder;
         }
+
+        var filePreview = FilePreview(
+          name: fileName,
+          icon: icon,
+          x: x,
+          y: y,
+        );
+        elements.add(filePreview);
+        index++;
       }
+
+
     }
 
     return elements;
@@ -47,8 +65,7 @@ class _FileExplorerState extends State<FileExplorer> {
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(top: 16),
-      child: GridView.count(
-        crossAxisCount: 6,
+      child: Stack(
         children: _generateElements(),
       ),
     );
--- lib/ui/components/previews/file_preview.dart
+++ lib/ui/components/previews/file_preview.dart
@@ -4,10 +4,14 @@ class FilePreview extends StatefulWidget {
   FilePreview({
     @required this.name,
     @required this.icon,
+    @required this.x,
+    @required this.y,
   });
 
   final String name;
   final IconData icon;
+  final double x;
+  final double y;
 
   @override
   _FilePreviewState createState() => _FilePreviewState();
@@ -16,25 +20,34 @@ class FilePreview extends StatefulWidget {
 class _FilePreviewState extends State<FilePreview> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          child: Icon(
-            widget.icon,
-            color: Colors.indigo,
-            size: 50,
-          ),
-        ),
-        Container(
-          child: Text(
-            widget.name,
-            style: TextStyle(
+    return Positioned(
+      left: widget.x,
+      top: widget.y,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            child: Icon(
+              widget.icon,
               color: Colors.indigo,
-              fontSize: 16
+              size: 50,
             ),
           ),
-        ),
-      ],
+          Container(
+            width: 100,
+            child: Text(
+              widget.name,
+              maxLines: 1,
+              textAlign: TextAlign.center,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                color: Colors.indigo,
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
